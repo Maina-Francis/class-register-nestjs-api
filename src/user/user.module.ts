f72eeede9699a11dca3a11366c144855fcfef2d7
francis <francismainakabutha@gmail.com>
2023-06-05 14:49:06 +0300

    updated the user module to include the teacher and admin resources

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AdminController } from './admin/admin.controller';
+import { TeacherController } from './teacher/teacher.controller';
+import { TeacherService } from './teacher/teacher.service';
+import { AdminService } from './admin/admin.service';
 
-@Module({})
+@Module({
+  controllers: [AdminController, TeacherController],
+  providers: [TeacherService, AdminService]
+})
 export class UserModule {}
